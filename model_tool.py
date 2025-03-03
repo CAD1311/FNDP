9637b0e14ecee45d21a5b0bae8e9f6246d6eafa1
tatoba <54407436+superboysb-12@users.noreply.github.com>
2025-03-03 22:38:32 +0800

    Update model_tool.py

--- model_tool.py
+++ model_tool.py
@@ -3,6 +3,39 @@ from qwen_vl_utils import process_vision_info
 
 
 
+# 下面是function calling的定义 网上偷的
+qwen_functions = [
+    {
+        "name_for_human": "谷歌搜索",
+        "name_for_model": "google_search",
+        "description_for_model": "谷歌搜索是一个通用搜索引擎，可用于访问互联网、查询百科知识、了解时事新闻等。"
+        + " Format the arguments as a JSON object.",
+        "parameters": [
+            {
+                "name": "search_query",
+                "description": "搜索关键词或短语",
+                "required": True,
+                "schema": {"type": "string"},
+            }
+        ],
+    },
+    {
+        "name_for_human": "文生图",
+        "name_for_model": "image_gen",
+        "description_for_model": "文生图是一个AI绘画（图像生成）服务，输入文本描述，返回根据文本作画得到的图片的URL。"
+        + " Format the arguments as a JSON object.",
+        "parameters": [
+            {
+                "name": "prompt",
+                "description": "英文关键词，描述了希望图像具有什么内容",
+                "required": True,
+                "schema": {"type": "string"},
+            }
+        ],
+    },
+]
+
+
 def generation_text(
     is_having_image: bool = False,
     title: str = "未知标题",
