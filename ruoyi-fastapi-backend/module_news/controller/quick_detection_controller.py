83b40fe36a1c1d9f1e93b7ebd5b939fb6c853f33
CAD1311 <y927053212@163.com>
2025-04-10 10:03:05 +0800

    update

--- ruoyi-fastapi-backend/module_news/controller/quick_detection_controller.py
+++ ruoyi-fastapi-backend/module_news/controller/quick_detection_controller.py
@@ -9,9 +9,13 @@ quick_detectionController = APIRouter(prefix='/quick', dependencies=[])
 def get_detection_service(request: Request) -> Detection_taskService:
     return request.app.state.detection_service
 
+class textDetectionRequest(BaseModel):
+    text : str
+
 @quick_detectionController.post('/detection')
 async def quick_detect_news_news_info(
-    text: str,
+    params: textDetectionRequest,
     service: Detection_taskService = Depends(get_detection_service),
 ):
+    text = params.text
     return service.quick_start_services(text)
